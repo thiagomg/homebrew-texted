96c06c7fc11937cd5075ab07435d26cb2ae17613
Thiago Massari Guedes <thiedri@gmail.com>
2024-09-05 23:19:39 -0700

    Added test

--- Formula/texted.rb
+++ Formula/texted.rb
@@ -14,15 +14,10 @@ class Texted < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test texted`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
-    system "false"
+    version_output = shell_output("#{bin}/texted --version").chomp
+    assert_equal "texted #{version}", version_output
+
+    version_output = shell_output("#{bin}/texted-tool --version").chomp
+    assert_equal "texted #{version}", version_output
   end
 end
