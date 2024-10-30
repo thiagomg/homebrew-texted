8a2ce69581c95664a25af98239ede260c42b219c
Thiago Massari Guedes <thiedri@gmail.com>
2024-10-29 21:18:58 -0700

    1.0.2

--- Formula/texted.rb
+++ Formula/texted.rb
@@ -2,8 +2,8 @@ class Texted < Formula
   desc "Markdown based blogging system where you own your content"
   homepage "https://thiagocafe.com/page/projects/"
   url "https://gitlab.com/thiagomg/texted.git",
-      tag:      "1.0.0",
-      revision: "90fb49e36946fbdf3352e60f2190b4d5eaeece89"
+      tag:      "1.0.2",
+      revision: "18d056db06c0705a81e198529ae41c7af130d18a"
   license "Apache-2.0"
   head "https://gitlab.com/thiagomg/texted.git", branch: "main"
 
