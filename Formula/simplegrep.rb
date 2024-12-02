c179e2be514252aa865e216b9bda951092fa1016
Thiago Massari Guedes <thiedri@gmail.com>
2024-12-01 17:23:12 -0800

    Version 1.0.4 added

--- Formula/simplegrep.rb
+++ Formula/simplegrep.rb
@@ -2,8 +2,8 @@ class Simplegrep < Formula
   desc "Grep alternative for those lazy to remember many command-line options"
   homepage "https://thiagocafe.com/page/projects/"
   url "https://gitlab.com/thiagomg/sgrep.git",
-      tag:      "1.0.3",
-      revision: "c580efbf56cc32afc8b24c40da585f020b9b51f3"
+      tag:      "1.0.4",
+      revision: "c47e271de3dd50ca49b865b2e6c9e51ad1c1a9c4"
   license "Apache-2.0"
   head "https://gitlab.com/thiagomg/sgrep.git", branch: "main"
 
@@ -14,15 +14,10 @@ class Simplegrep < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test simplegrep`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
-    system "false"
+    output = shell_output("#{bin}/sgrep --version")
+    assert_match "sgrep 1.0.4", output
+
+    output = shell_output("echo 'XYZ' | #{bin}/sgrep -p 'X' --raw")
+    assert_match "XYZ", output
   end
 end
