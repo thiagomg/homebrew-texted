1a034147536d311c46291c34e47b705f0409cf5c
Thiago Massari Guedes <thiedri@gmail.com>
2025-04-22 00:12:11 -0700

    New sgrep version

--- Formula/simplegrep.rb
+++ Formula/simplegrep.rb
@@ -2,8 +2,8 @@ class Simplegrep < Formula
   desc "Grep alternative for those lazy to remember many command-line options"
   homepage "https://thiagocafe.com/page/projects/"
   url "https://gitlab.com/thiagomg/sgrep.git",
-      tag:      "1.0.5",
-      revision: "2f304bfc16a13f2b81d239df89b27d705cd1b39e"
+      tag:      "1.0.7",
+      revision: "a7b5ead590d8371e4f9ab9801be1158fd8c92c33"
   license "Apache-2.0"
   head "https://gitlab.com/thiagomg/sgrep.git", branch: "main"
 
@@ -15,7 +15,7 @@ class Simplegrep < Formula
 
   test do
     output = shell_output("#{bin}/sgrep --version")
-    assert_match "sgrep 1.0.5", output
+    assert_match "sgrep 1.0.7", output
 
     output = shell_output("echo 'XYZ' | #{bin}/sgrep -p 'X' --raw")
     assert_match "XYZ", output
